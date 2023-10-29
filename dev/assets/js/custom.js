c58f67f6f7b7d3ef32759ec447b1412416ac1b75
Vitalii Podobiedov <v.podobiedov@gmail.com>
2023-10-29 16:57:24 +0200

    calc-form desktop&mobile

--- dev/assets/js/custom.js
+++ dev/assets/js/custom.js
@@ -9,8 +9,28 @@ $('.first-screen-slider').slick({
     dots: true,
     infinite: true,
     appendArrows: '.first-screen-nav',
+    responsive: [
+        {
+          breakpoint: 991,
+          settings: {
+            arrows: false,
+          }
+        },
+      ]
 });
 
+//upload-file
+
+document.getElementById('fileInput').onchange = function () {
+  document.getElementById('file-name').innerHTML = this.files[0].name;
+}
+
+
+const checkButton = document.querySelectorAll('[type="checkbox"]');
+
+checkButton.forEach((item, index) => {
+  index === 0? item.closest('.input-box').classList.add ('pos-checkbox'): null;
+})
 
 // $(document).ready(function() {
 //     $('.first-screen').slick({
