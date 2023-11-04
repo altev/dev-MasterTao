9222bda9d948423e8a7c585e107f9e2fbedb2e73
Vitalii Podobiedov <v.podobiedov@gmail.com>
2023-11-04 11:00:49 +0200

    advantages desktop&mobile

--- dev/assets/js/custom.js
+++ dev/assets/js/custom.js
@@ -32,6 +32,34 @@ checkButton.forEach((item, index) => {
   index === 0? item.closest('.input-box').classList.add ('pos-checkbox'): null;
 })
 
+mobileOnlySlider(".advantages-slider", true, false, 991);
+
+function mobileOnlySlider($slidername, $dots, $arrows, $breakpoint) {
+  var slider = $($slidername);
+  var settings = {
+    mobileFirst: true,
+    dots: $dots,
+    arrows: $arrows,
+    responsive: [
+      {
+        breakpoint: $breakpoint,
+        settings: "unslick"
+      }
+    ]
+  };
+
+  slider.slick(settings);
+
+  $(window).on("resize", function () {
+    if ($(window).width() > $breakpoint) {
+      return;
+    }
+    if (!slider.hasClass("slick-initialized")) {
+      return slider.slick(settings);
+    }
+  });
+} // Mobile Only Slider
+
 // $(document).ready(function() {
 //     $('.first-screen').slick({
 //         infinite: true,
